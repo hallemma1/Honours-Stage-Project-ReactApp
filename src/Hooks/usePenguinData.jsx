3b477fdc7a9abe6e0db327630eb13aa0ddf542e5
hallemma1 <91595251+hallemma1@users.noreply.github.com>
2024-04-03 15:46:10 +0100

    connection to server

--- src/Hooks/usePenguinData.jsx
+++ src/Hooks/usePenguinData.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5108/api',
+    baseURL: 'https://penguinserver.azurewebsites.net/api',
 });
 
 export const usePenguinData = () => {
